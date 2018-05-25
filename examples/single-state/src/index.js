1707a7dd119885a27cc131d4ef67a965c9a66d56
wxnet2013 <wxnet2013@gmail.com>
2018-05-25 18:30:34 +0800

    docs: add single state demo

---
+++ examples/single-state/src/index.js
@@ -0,0 +1,96 @@
+import rxLoop from 'rxloop';
+import { Observable, of, from } from 'rxjs';
+import { switchMap, map, combineLatest } from 'rxjs/operators';
+
+const getCounterApi = () => Promise.resolve(100);
+
+const app = rxLoop();
+
+app.model({
+  name: 'counter',
+  state: {
+    counter: 0,
+  },
+  reducers: {
+    inc(state, action) {
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    },
+    init(state, action) {
+      return {
+        ...state,
+        counter: action.counter,
+      };
+    },
+  },
+  epics: {
+    getCounter(action$, cancel$) {
+      return action$.pipe(
+        switchMap(() => {
+          return from(
+            getCounterApi()
+          );
+        }),
+        map((data) => {
+          return {
+            type: 'init',
+            counter: data,
+          }
+        }),
+      );
+    }
+  },
+});
+
+
+app.model({
+  name: 'user',
+  state: {
+    name: 'wxnet',
+    email: 'www@ddd.com',
+  },
+});
+
+const user$ = app.stream('user');
+const counter$ = app.stream('counter');
+
+user$.pipe(
+  combineLatest(
+    counter$,
+  ),
+  map(([user, counter]) => {
+    return {
+      user,
+      counter,
+    };
+  }),
+)
+.subscribe(state => {
+  console.log(state);
+});
+
+
+
+// counter$.subscribe((state) => {
+//   console.log(state);
+//   // this.setState(state);
+// });
+
+app.dispatch({
+  type: 'counter/getCounter',
+});
+
+app.dispatch({
+  type: 'counter/inc',
+});
+// app.dispatch({
+//   type: 'counter/inc',
+// });
+// app.dispatch({
+//   type: 'counter/inc',
+// });
+// app.dispatch({
+//   type: 'counter/inc',
+// });
