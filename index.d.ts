8638f9da0203b388c96bb6741f2cf49d3118ad94
wxnet2013 <wxnet2013@gmail.com>
2018-08-27 19:49:58 +0800

    fix: support ts

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,40 @@
 import { Observable } from 'rxjs';
 
-export interface Model {
-  name: string,
-  state?: any,
-  reducers?: any,
-  epics?: any,
-}
-
 export interface Action {
   type: string,
   payload?: any,
   data?: any,
 }
 
+export type Reducer = (state: any, action: Action) => any;
+
+export type Epic = (action$: Observable<any>, cancel$: Observable<any>) => Observable<any>;
+
+export interface EpicsMapObject {
+  [key: string]: Epic,
+}
+
+export interface ReducersMapObject {
+  [key: string]: Reducer,
+}
+
+export interface Model {
+  name: string,
+  state?: any,
+  reducers?: ReducersMapObject,
+  epics?: EpicsMapObject,
+}
+
 export interface RxLoopInstance {
   model: (model: Model) => void,
   stream: (modelName: String) => Observable<any>,
   dispatch: (action: Action) => void,
+  getState: (modelName: String) => any,
+  next: (action: Action) => void,
 }
 
-export default function rxloop(): RxLoopInstance;
+export interface Config {
+  plugins?: Array<function>,
+}
+
+export default function rxloop(conf?: Config): RxLoopInstance;
