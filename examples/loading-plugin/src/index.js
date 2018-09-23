1ce0821b993764c12d4396f879a12de3ed730194
wxnet2013 <wxnet2013@gmail.com>
2018-09-24 03:22:15 +0800

    docs: update loading examples

--- examples/loading-plugin/src/index.js
+++ examples/loading-plugin/src/index.js
@@ -1,11 +1,11 @@
 import { from } from 'rxjs';
-import { mergeMap, map, takeUntil } from 'rxjs/operators';
+import { mergeMap, map } from 'rxjs/operators';
 import rxloop from '@rxloop/core';
 import loading from '@rxloop/loading';
 
 const apiSlow = async () => {
   const data = await new Promise((resolve) => {
-    setTimeout(() => resolve({}), 2000);
+    setTimeout(() => resolve({}), 1000);
   });
   return { code: 200, data };
 };
@@ -59,42 +59,17 @@ const app = rxloop({
   plugins: [ loading() ],
 });
 app.model(counter);
+app.start();
 
 app.stream('counter').subscribe(state => {
-  document.getElementById('counter').innerHTML = state.counter;
+  document.getElementById('counter').innerHTML = state.loading.getData ? 'loading' : state.counter;
 });
 
 // loading 状态
 app.stream('loading').subscribe(state => {
   // 某个 epic 的 loading 状态
   console.log(state.epics.counter);
-  document.getElementById('counter').innerHTML = state.epics.counter.getData ? `
-  <svg width="28" height="28" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" stroke="#3498DB">
-    <g fill="none" fill-rule="evenodd">
-        <g transform="translate(1 1)" stroke-width="2">
-            <circle stroke-opacity=".5" cx="18" cy="18" r="18"/>
-            <path d="M36 18c0-9.94-8.06-18-18-18">
-                <animateTransform
-                    attributeName="transform"
-                    type="rotate"
-                    from="0 18 18"
-                    to="360 18 18"
-                    dur="1s"
-                    repeatCount="indefinite"/>
-            </path>
-        </g>
-    </g>
-</svg>
-`:
-'';
-
-  // TODO： 某个 model 的 loading 状态，支持获取异步个数
-  // state.counter
-
-  // TODO： 整个应用的 loading 状态，支持获取异步个数
-  // state.global
 });
-
 
 document.getElementById('getdata').onclick = () => {
   app.dispatch({
