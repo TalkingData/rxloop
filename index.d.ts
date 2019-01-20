4977bc274971d67c0859d2034d93a8f9e9cf4968
yongxiang.bao <yongxiang.bao@mobilemd.cn>
2019-01-20 23:38:17 +0800

    feat: class style model

--- index.d.ts
+++ index.d.ts
@@ -15,31 +15,45 @@ export interface OperatorsMap {
   cancel$: Observable<Action>,
 }
 
-export type Reducer = (state: any, action: Action) => any;
+export type Reducer<T=any> = (state: T, action?: Action) => T;
 
-export type Pipe = (action$: Observable<Action>, operators: OperatorsMap) => Observable<Action>;
+export type Pipe = (action$: Observable<Action>, operators?: OperatorsMap) => Observable<Action>;
 
-export interface PipesMapObject {
-  [key: string]: Pipe,
+type MapObject<T>={
+  [key: string]: T,
 }
 
-export interface ReducersMapObject {
-  [key: string]: Reducer,
-}
-
-export interface Model {
+export type Model<T=any> = {
   name: string,
-  state?: any,
-  reducers?: ReducersMapObject,
-  pipes?: PipesMapObject,
+  state: T,
+  reducers?: MapObject<Reducer>,
+  pipes?: MapObject<Pipe>,
 }
+
+interface NumberFunction extends Function {
+  (n:number):number;
+}
+
+type Class = { new(): any; };
+
+// decorator
+export function decorators<T=any>():{
+  model(state: T): (Target: Class) => Class
+ 
+  reducer(target: any, name: string, descriptor: TypedPropertyDescriptor<Reducer<T>>) : void
+ 
+  pipe(target: any, name: string, descriptor: TypedPropertyDescriptor<Pipe>) : void 
+ 
+}
+ 
+
 
 export interface Unsubscribe {
   (): void
 }
 
 export interface RxLoopInstance {
-  model: (model: Model) => void,
+  model: (model: Model | Class) => void,
   stream: (modelName: String) => Observable<any>,
   dispatch: (action: Action) => void,
   subscribe: (listener: () => void) => Unsubscribe,
